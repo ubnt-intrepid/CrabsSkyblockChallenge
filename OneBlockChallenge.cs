95775104486f86d401c015d71044c19d2b2d701b
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2021-10-05 13:55:25 +0900

    initial commit

---
+++ OneBlockChallenge.cs
@@ -0,0 +1,215 @@
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace OneBlockChallenge
+{
+    public class OneBlockChallenge : Mod
+    {
+        public override void AddRecipes()
+        {
+            CreateRecipe(ItemID.SandBlock)
+                .AddIngredient(ItemID.HardenedSand)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+
+            CreateRecipe(ItemID.SnowBlock)
+                .AddIngredient(ItemID.IceBlock)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+
+            CreateRecipe(ItemID.SiltBlock, amount: 2)
+                .AddIngredient(ItemID.SandBlock)
+                .AddIngredient(ItemID.ClayBlock)
+                .AddTile(TileID.Furnaces)
+                .Register();
+
+            CreateRecipe(ItemID.SlushBlock, amount: 2)
+                .AddIngredient(ItemID.SnowBlock)
+                .AddIngredient(ItemID.MudBlock)
+                .AddTile(TileID.Furnaces)
+                .Register();
+
+            CreateRecipe(ItemID.Hive)
+                .AddIngredient(ItemID.Wood, stack: 5) // FIXME: available for any wood
+                .AddCondition(Recipe.Condition.NearHoney)
+                .Register();
+
+            CreateRecipe(ItemID.Hellforge)
+                .AddIngredient(ItemID.Furnace)
+                .AddIngredient(ItemID.Hellstone, stack: 30)
+                .AddIngredient(ItemID.Obsidian, stack: 30)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+    }
+
+    public class OBCPlayer : ModPlayer
+    {
+        public override IEnumerable<Item> AddStartingItems(bool mediumCoreDeath)
+        {
+            if (mediumCoreDeath)
+            {
+                return Enumerable.Empty<Item>();
+            }
+
+            return new[]
+            {
+                new Item(ItemID.Acorn, stack: 5),
+                new Item(ItemID.GrassSeeds, stack: 1),
+                new Item(ItemID.JungleGrassSeeds, stack: 1),
+                new Item(ItemID.MushroomGrassSeeds, stack: 1),
+                new Item(ItemID.WaterBucket, stack: 1),
+                new Item(ItemID.HoneyBucket, stack: 1),
+                new Item(ItemID.Torch, stack: 1),
+            };
+        }
+    }
+
+    public class OBCWorld : ModSystem
+    {
+        public static bool defeatBoss2 = false;   // Eater of World / Brain of Cthulhu
+        public static bool defeatSkeletron = false;
+        public static bool defeatWoF = false;
+        public static bool defeatPlantera = false;
+
+        public override void LoadWorldData(TagCompound tags)
+        {
+            TryGetTag(tags, "defeatBoss2", out defeatBoss2);
+            TryGetTag(tags, "defeatSkeletron", out defeatSkeletron);
+            TryGetTag(tags, "defeatWoF", out defeatWoF);
+            TryGetTag(tags, "defeatPlantera", out defeatPlantera);
+        }
+
+        public override void SaveWorldData(TagCompound tags)
+        {
+            tags.Set("defeatBoss2", defeatBoss2);
+            tags.Set("defeatSkeletron", defeatSkeletron);
+            tags.Set("defeatWoF", defeatWoF);
+            tags.Set("defeatPlantera", defeatPlantera);
+        }
+
+        static bool TryGetTag<T>(TagCompound me, string name, out T dest)
+        {
+            if (me.ContainsKey(name))
+            {
+                dest = me.Get<T>(name);
+                return true;
+            }
+            else
+            {
+                dest = default(T);
+                return false;
+            }
+        }
+    }
+
+    public class OBCGlobalNPC : GlobalNPC
+    {
+        public override void SetupShop(int type, Chest shop, ref int nextSlot)
+        {
+            switch (type)
+            {
+                case NPCID.Merchant:
+                    shop.item[nextSlot].SetDefaults(ItemID.Extractinator);
+                    nextSlot++;
+
+                    shop.item[nextSlot].SetDefaults(ItemID.LifeCrystal);
+                    nextSlot++;
+
+                    shop.item[nextSlot].SetDefaults(ItemID.Cobweb);
+                    shop.item[nextSlot].shopCustomPrice = Item.buyPrice(copper: 10);
+                    nextSlot++;
+
+                    if (OBCWorld.defeatPlantera)
+                    {
+                        shop.item[nextSlot].SetDefaults(ItemID.LihzahrdAltar);
+                        nextSlot++;
+                    }
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public override void OnKill(NPC npc)
+        {
+            switch (npc.type)
+            {
+                case NPCID.EaterofWorldsHead:
+                case NPCID.EaterofWorldsBody:
+                case NPCID.EaterofWorldsTail:
+                case NPCID.BrainofCthulhu:
+                    NPC.SetEventFlagCleared(ref OBCWorld.defeatBoss2, -1);
+                    break;
+
+                case NPCID.SkeletronHead:
+                    NPC.SetEventFlagCleared(ref OBCWorld.defeatSkeletron, -1);
+                    break;
+
+                case NPCID.WallofFlesh:
+                    NPC.SetEventFlagCleared(ref OBCWorld.defeatWoF, -1);
+                    break;
+
+                case NPCID.Plantera:
+                    NPC.SetEventFlagCleared(ref OBCWorld.defeatPlantera, -1);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    public class OBCGlobalItem : GlobalItem
+    {
+        static int[] tier1Ores = new int[] { ItemID.CopperOre, ItemID.TinOre, ItemID.IronOre, ItemID.LeadOre, ItemID.CobaltOre, ItemID.PalladiumOre };
+        static int[] tier2Ores = new int[] { ItemID.SilverOre, ItemID.TungstenOre, ItemID.MythrilOre, ItemID.OrichalcumOre };
+        static int[] tier3Ores = new int[] { ItemID.GoldOre, ItemID.PlatinumOre, ItemID.AdamantiteOre, ItemID.TitaniumOre };
+
+        public override void ExtractinatorUse(int extractType, ref int resultType, ref int resultStack)
+        {
+            if (!OBCWorld.defeatWoF)
+            {
+                // pre hardmode
+                return;
+            }
+
+            switch (resultType)
+            {
+                case ItemID.CopperOre:
+                case ItemID.TinOre:
+                case ItemID.IronOre:
+                case ItemID.LeadOre:
+                    resultType = Utils.Choice(tier1Ores);
+                    break;
+
+                case ItemID.SilverOre:
+                case ItemID.TungstenOre:
+                    resultType = Utils.Choice(tier2Ores);
+                    break;
+
+                case ItemID.GoldOre:
+                case ItemID.PlatinumOre:
+                    resultType = Utils.Choice(tier3Ores);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    public class Utils {
+        public static T Choice<T>(T[] array)
+        {
+            var index = Main.rand.Next(0, array.Length);
+            return array[index];
+        }
+    }
+}
---
+++ Tiles/InfiniteBlock.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace OneBlockChallenge.Tiles
+{
+    public class InfiniteBlock : ModTile
+    {
+        public override string Texture => $"Terraria/Images/Tiles_{TileID.Cloud}";
+
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = false;
+            Main.tileBlockLight[Type] = false;
+            Main.tileNoSunLight[Type] = false;
+
+            DustType = DustID.Cloud;
+            AdjTiles = new int[] { TileID.DemonAltar };
+
+            TileID.Sets.DoesntGetReplacedWithTileReplacement[Type] = true;
+
+            AddMapEntry(new Color(255, 255, 200));
+        }
+
+        public override bool CanExplode(int i, int j) => false;
+
+        public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
+        {
+            if (!fail)
+            {
+                fail = true;
+
+                var item = NextBlock();
+
+                int num = Item.NewItem(i * 16, j * 16, 12, 12, item, 1, noBroadcast: false, -1);
+                if (Main.netMode == NetmodeID.MultiplayerClient)
+                {
+                    NetMessage.SendData(MessageID.SyncItem, -1, -1, null, num, 1f);
+                }
+            }
+        }
+
+        static int NextBlock()
+        {
+            // TODO: optimize
+            var drawnBlocks = new List<int> {
+                ItemID.DirtBlock,
+                ItemID.StoneBlock,
+                ItemID.ClayBlock,
+                ItemID.HardenedSand,
+                ItemID.IceBlock,
+            };
+
+            if (OBCWorld.defeatBoss2)
+            {
+                drawnBlocks.Add(ItemID.Hellstone);
+            }
+
+            if (OBCWorld.defeatSkeletron)
+            {
+                drawnBlocks.AddRange(new int[] {
+                    ItemID.BlueBrick,
+                    ItemID.GreenBrick,
+                    ItemID.PinkBrick,
+                });
+            }
+
+            if (OBCWorld.defeatPlantera)
+            {
+                drawnBlocks.Add(ItemID.LihzahrdBrick);
+            }
+
+            var index = Main.rand.Next(0, drawnBlocks.Count);
+            return drawnBlocks[index];
+        }
+
+        public override bool RightClick(int i, int j)
+        {
+            Main.NewText("Current sources:", Color.Magenta);
+            Main.NewText("  - Basic blocks (Dirt, Stone, Clay, Hardened Sand, Ice)");
+            if (OBCWorld.defeatBoss2)
+            {
+                Main.NewText("  - Hellstone");
+            }
+            if (OBCWorld.defeatSkeletron)
+            {
+                Main.NewText("  - Dungeon bricks");
+            }
+            if (OBCWorld.defeatPlantera)
+            {
+                Main.NewText("  - Lihzahrd brick");
+            }
+
+            return true;
+        }
+    }
+}
