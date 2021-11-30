9200cc80bbb68c85bae8b57c1c67d4ffcf65516f
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2021-11-30 16:59:03 +0900

    drop Unsafe Wall items from naturally-generated walls

---
+++ Walls/SkyblockGlobalWall.cs
@@ -0,0 +1,27 @@
+﻿using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace CrabsSkyblockChallenge.Walls
+{
+    public class SkyblockGlobalWall : GlobalWall
+    {
+        public override bool Drop(int i, int j, int type, ref int dropType)
+        {
+            switch (type)
+            {
+                case WallID.Sandstone:
+                    dropType = ModContent.ItemType<Items.UnsafeSandstoneWall>();
+                    break;
+
+                case WallID.SpiderUnsafe:
+                    dropType = ModContent.ItemType<Items.UnsafeSpiderWall>();
+                    break;
+
+                default:
+                    break;
+            }
+
+            return true;
+        }
+    }
+}
