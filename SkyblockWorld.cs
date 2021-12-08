868eb0842278c3dfbb8da995afd7dc0bb085e4c5
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2021-12-08 18:35:07 +0900

    acquiring water makes harder in all world seeds

--- SkyblockWorld.cs
+++ SkyblockWorld.cs
@@ -440,17 +440,15 @@ namespace CrabsSkyblockChallenge
         {
             PlaceTile(new[] {         -6, -5, -4,                                  7, 8        }, -1, TileID.Mud);
             PlaceTile(new[] { -8, -7, -6, -5, -4, -3, -2,                    5, 6, 7, 8, 9, 10 },  0, TileID.Mud);
-            PlaceTile(new[] { -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2,    4, 5, 6, 7, 8, 9, 10 },  1, TileID.Mud);
+            PlaceTile(new[] { -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },  1, TileID.Mud);
             PlaceTile(new[] {                 -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9     },  2, TileID.Mud);
             PlaceTile(new[] {                         -2, -1, 0, 1, 2, 3, 4, 5,    7           },  3, TileID.Mud);
 
             PlaceTile(new[] {         -6, -5, -4,                                  7, 8        }, -1, TileID.JungleGrass);
             PlaceTile(new[] { -8, -7, -6,     -4, -3, -2,                    5, 6, 7, 8, 9, 10 },  0, TileID.JungleGrass);
-            PlaceTile(new[] { -8, -7, -6, -5, -4,     -2, -1, 0, 1, 2,    4, 5,          9, 10 },  1, TileID.JungleGrass);
+            PlaceTile(new[] { -8, -7, -6, -5, -4,     -2, -1, 0, 1, 2, 3, 4, 5,          9, 10 },  1, TileID.JungleGrass);
             PlaceTile(new[] {                 -4, -3, -2,           2, 3, 4, 5, 6, 7, 8, 9     },  2, TileID.JungleGrass);
             PlaceTile(new[] {                         -2, -1, 0, 1, 2, 3, 4, 5,    7           },  3, TileID.JungleGrass);
-
-            PlaceLiquid(3, 1, WorldGen.getGoodWorldGen ? (byte)LiquidID.Lava : WorldGen.notTheBees ? (byte)LiquidID.Honey : (byte)LiquidID.Water);
 
             // Hornet Statue
             PlaceTile(-1, 0, TileID.Statues, style: 16);
